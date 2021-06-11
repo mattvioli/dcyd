95b895294e67a8a20bb3a1ee69f2b95f0eec7653
Matt <mattvioli@gmail.com>
2021-06-11 14:25:24 +1000

    added create post, main text does not go to db

--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -2,3 +2,16 @@ get '/posts' do
   results = posts_data()
   erb :'posts/index', locals: { posts: results }
 end
+
+get '/create' do
+  erb :'posts/new'
+end
+
+post '/create_post' do
+  title = params[:title]
+  main_text = params[:main_text]
+  user_id = session[:user_id]
+  create_post(title, main_text, user_id)
+
+  redirect '/posts'
+end
--- models/posts.rb
+++ models/posts.rb
@@ -2,3 +2,7 @@ def posts_data
   run_sql("SELECT * FROM posts;")
 end
 # need to add inner join to user ID/username
+
+def create_post(title, main_text, user_id)
+  run_sql("INSERT INTO posts (title, main_text, user_id) VALUES ($1, $2, $3)", [title, main_text, user_id])
+end
