d9e31638a9a1ce553cc614b7d6812abb7635efc9
Matt <mattvioli@gmail.com>
2021-06-15 21:46:43 +1000

    quick vote button and some style

--- controllers/answers_controller.rb
+++ controllers/answers_controller.rb
@@ -6,3 +6,11 @@ post '/reply/:post_id' do |post_id|
   
   redirect '/posts'
 end
+
+post '/quick_reply/:reply_id' do |reply_id|
+  results = get_reply_from_id(reply_id)
+    
+  create_reply(results[0]["reply"], session[:user_id], results[0]["post_id"])
+
+  redirect '/posts'
+end
