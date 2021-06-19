5dca2bcf704d006b10fd6eee4f94dbeaf433ece7
Matt <mattvioli@gmail.com>
2021-06-19 12:43:43 +1000

    chevron shows proof

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,5 +6,12 @@ elements.forEach((item) => {
 
 function showProof(e) {
   e.target.classList.toggle("flip-horizontally");
+  let proofImg = document.querySelector(`#proof${e.target.id}`);
+
+  if (proofImg.style.display === "") {
+    proofImg.style.display = "block";
+  } else {
+    proofImg.style.display = "";
+  }
   console.log(e.target.id);
 }
